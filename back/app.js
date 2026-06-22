cb905431c711554be69c21173ce6eed24e16dce1
dncworking <dominyka.dobuzinskaite@gmail.com>
2026-06-22 14:00:51 +0300

    feat: install cors

--- back/app.js
+++ back/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import cityRouter from "./routes/cityRoutes.js";
 import placeRouter from "./routes/placeRoutes.js";
 import authRouter from "./routes/authRoutes.js";
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/v1/cities", cityRouter);
